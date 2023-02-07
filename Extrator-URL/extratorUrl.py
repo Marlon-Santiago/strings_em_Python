df1d0eb2a31cf2c41ac50bfb98bcf86e52044654
Fawke-s <83735637+Marlon-Santiago@users.noreply.github.com>
2023-02-06 22:30:49 -0300

    sumario

--- Extrator-URL/extratorUrl.py
+++ Extrator-URL/extratorUrl.py
@@ -1,39 +1,56 @@
-class Extrator_URL:
+import re
 
+
+class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
-        self.valida_url
+        self.valida_url()
 
     def sanitiza_url(self, url):
         if type(url) == str:
             return url.strip()
         else:
-            return ''
+            return ""
 
     def valida_url(self):
         if not self.url:
-            raise ValueError('A URL está vazia')
+            raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
-        indice_interrogação = self.url.find('?')
-        url_base = self.url[:indice_interrogação]
+        indice_interrogacao = self.url.find('?')
+        url_base = self.url[:indice_interrogacao]
         return url_base
 
-    def get_parametros(self):
-        indice_interrogação = self.url.find('?')
-        url_parametros = self.url[indice_interrogação+1:]
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find('?')
+        url_parametros = self.url[indice_interrogacao + 1:]
         return url_parametros
 
-    def get_parametro_busca(self, parametro_busca):
-        indice_parametro = self.get_parametros().find(parametro_busca)
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
         indice_valor = indice_parametro + len(parametro_busca) + 1
-        indice_e_comercial = self.get_parametros().find('&', indice_valor)
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
         if indice_e_comercial == -1:
-            valor = self.get_parametros()[indice_valor:]
+            valor = self.get_url_parametros()[indice_valor:]
         else:
-            valor = self.get_parametros()[indice_valor:indice_e_comercial]
+            valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
+    
+    def __len__(self):
+        return len(self.url)
+    
+    def __str__(self):
+        return self.url
 
-extrator_url = Extrator_URL('bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real')
-valor_quantidade = extrator_url.get_parametro_busca('quantidade')
+    
+
+url = "byteb8ank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
+
